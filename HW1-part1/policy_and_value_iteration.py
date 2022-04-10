af209930494af5d91d746174e391990372ceb419
William <39876589+wei032499@users.noreply.github.com>
2022-04-10 15:29:43 +0800

    HW1-part1

---
+++ HW1-part1/policy_and_value_iteration.py
@@ -0,0 +1,243 @@
+# Spring 2022, IOC 5259 Reinforcement Learning
+# HW1, partI: Policy Iteration and Value iteration for MDPs
+       
+import numpy as np
+import gym
+
+def get_rewards_and_transitions_from_env(env):
+    # Get state and action space sizes
+    num_states = env.observation_space.n
+    num_actions = env.action_space.n
+
+    # Intiailize matrices
+    R = np.zeros((num_states, num_actions, num_states))
+    P = np.zeros((num_states, num_actions, num_states))
+
+    # Get rewards and transition probabilitites for all transitions from an OpenAI gym environment
+    for s in range(num_states):
+        for a in range(num_actions):
+            for transition in env.P[s][a]:
+                prob, s_, r, done = transition
+                R[s, a, s_] = r
+                P[s, a, s_] = prob
+                
+    return R, P
+
+def value_iteration(env, gamma=0.9, max_iterations=10**6, eps=10**-3):
+    """        
+        Run value iteration (You probably need no more than 30 lines)
+        
+        Input Arguments
+        ----------
+            env: 
+                the target environment
+            gamma: float
+                the discount factor for rewards
+            max_iterations: int
+                maximum number of iterations for value iteration
+            eps: float
+                for the termination criterion of value iteration 
+        ----------
+        
+        Output
+        ----------
+            policy: np.array of size (500,)
+        ----------
+        
+        TODOs
+        ----------
+            1. Initialize the value function V(s)
+            2. Get transition probabilities and reward function from the gym env
+            3. Iterate and improve V(s) using the Bellman optimality operator
+            4. Derive the optimal policy using V(s)
+        ----------
+    """
+    num_spaces = env.observation_space.n
+    num_actions = env.action_space.n
+    
+    # Initialize with a random policy
+    policy = np.array([env.action_space.sample() for _ in range(num_spaces)])
+    
+    ##### FINISH TODOS HERE #####
+
+    V = np.zeros(num_spaces)
+
+    for _ in range(max_iterations):
+        delta = 0
+        for s in range(num_spaces):
+            q_s = np.zeros(num_actions)
+            for a in range(num_actions):
+                for prob, next_state, reward, done in env.P[s][a]:
+                    if done:
+                        q_s[a] += prob * reward
+                    else:
+                        q_s[a] += (prob * (reward + gamma * V[next_state]))
+            best_value = max(q_s)
+            delta = max(delta, np.abs(best_value - V[s]))
+            V[s] = best_value
+            policy[s] = np.argmax(q_s)
+        if delta < eps:
+            break
+
+    #############################
+    
+    # Return optimal policy    
+    return policy
+
+def policy_evaluation(env, V, policy, gamma, eps):
+
+    num_spaces = env.observation_space.n
+    num_actions = env.action_space.n
+
+    while True:
+        delta = 0
+        for s in range(num_spaces):
+            v = 0
+            for prob, next_state, reward, done in env.P[s][policy[s]]:
+                if done:
+                    v += prob * reward
+                else:
+                    v += (prob * (reward + gamma * V[next_state]))
+            
+            delta = max(delta, np.abs(v - V[s]))
+            V[s] = v
+        if delta < eps:
+            break
+
+def policy_improvement(env, V, policy, gamma):
+
+    num_spaces = env.observation_space.n
+    num_actions = env.action_space.n
+
+    stable = True
+   
+    for s in range(num_spaces):
+        q_s = np.zeros(num_actions)
+        for a in range(num_actions):
+            for prob, next_state, reward, done in env.P[s][a]:
+                if done:
+                    q_s[a] += prob * reward
+                else:
+                    q_s[a] += (prob * (reward + gamma * V[next_state]))
+        policy_new = np.argmax(q_s)
+        if policy_new != policy[s]:
+            stable = False
+        policy[s] = policy_new
+    
+    return stable
+        
+
+def policy_iteration(env, gamma=0.9, max_iterations=10**6, eps=10**-3):
+    """ 
+        Run policy iteration (You probably need no more than 30 lines)
+        
+        Input Arguments
+        ----------
+            env: 
+                the target environment
+            gamma: float
+                the discount factor for rewards
+            max_iterations: int
+                maximum number of iterations for the policy evalaution in policy iteration
+            eps: float
+                for the termination criterion of policy evaluation 
+        ----------  
+        
+        Output
+        ----------
+            policy: np.array of size (500,)
+        ----------
+        
+        TODOs
+        ----------
+            1. Initialize with a random policy and initial value function
+            2. Get transition probabilities and reward function from the gym env
+            3. Iterate and improve the policy
+        ----------
+    """
+    num_spaces = env.observation_space.n
+    num_actions = env.action_space.n
+    
+    # Initialize with a random policy
+    policy = np.array([env.action_space.sample() for _ in range(num_spaces)])
+    
+    ##### FINISH TODOS HERE #####
+
+    V = np.zeros(num_spaces)
+
+    for _ in range(max_iterations):  
+        policy_evaluation(env, V, policy, gamma, eps)
+        stable = policy_improvement(env, V, policy, gamma)
+        if stable:
+            # print(_)
+            break
+    
+
+    #############################
+
+    # Return optimal policy
+    return policy
+
+def print_policy(policy, mapping=None, shape=(0,)):
+    print(np.array([mapping[action] for action in policy]).reshape(shape))
+
+
+def run_pi_and_vi(env_name):
+    """ 
+        Enforce policy iteration and value iteration
+    """    
+    env = gym.make(env_name)
+    print('== {} =='.format(env_name))
+    print('# of actions:', env.action_space.n)
+    print('# of states:', env.observation_space.n)
+    print(env.desc)
+
+    vi_policy = value_iteration(env)
+    pi_policy = policy_iteration(env)
+
+    return pi_policy, vi_policy
+
+def play(env_name, policy):
+    import os
+    import time
+    env = gym.make(env_name)
+    observation = env.reset()
+    step = 0
+    while True:
+        os.system("cls")
+        env.render()
+        print(f"steps: {step}")
+        action = policy[observation]
+        observation, reward, done, info = env.step(action)
+        step += 1
+        time.sleep(0.5)
+        if done:
+            os.system("cls")
+            env.render()
+            print(f"steps: {step}")
+            print("END!!")
+            time.sleep(1)
+            observation = env.reset()
+            step = 0
+            # break
+    env.close()
+
+if __name__ == '__main__':
+    # OpenAI gym environment: Taxi-v2 or Taxi-v3
+    pi_policy, vi_policy = run_pi_and_vi('Taxi-v3')
+
+    # For debugging
+    action_map = {0: "S", 1: "N", 2: "E", 3: "W", 4: "P", 5: "D"}
+    print_policy(pi_policy, action_map, shape=None)
+    print_policy(vi_policy, action_map, shape=None)
+    
+    # Compare the policies obatined via policy iteration and value iteration
+    diff = sum([abs(x-y) for x, y in zip(pi_policy.flatten(), vi_policy.flatten())])        
+    print('Discrepancy:', diff)
+
+    # play('Taxi-v3', pi_policy)
+    
+    
+
+
+
